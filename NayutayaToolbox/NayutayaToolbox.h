67a3b243dd9ba97c4e90af10ed1c6ac80d7e0ce6
Yuya Kato <yuyakato@gmail.com>
2015-10-22 13:53:54 +0900

    Initial Commit

---
+++ NayutayaToolbox/NayutayaToolbox.h
@@ -0,0 +1,19 @@
+//
+//  NayutayaToolbox.h
+//  NayutayaToolbox
+//
+//  Created by Yuya Kato on 2015/10/22.
+//  Copyright © 2015年 Nayutaya Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NayutayaToolbox.
+FOUNDATION_EXPORT double NayutayaToolboxVersionNumber;
+
+//! Project version string for NayutayaToolbox.
+FOUNDATION_EXPORT const unsigned char NayutayaToolboxVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NayutayaToolbox/PublicHeader.h>
+
+

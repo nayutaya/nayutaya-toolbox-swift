bda3b25b5414fd6adb95501f8ba7a9aa4a7d30ca
Yuya Kato <yuyakato@gmail.com>
2015-10-22 14:03:47 +0900

    DeviceEnvironmentクラスのひな形を追加

---
+++ NayutayaToolboxTests/DeviceEnvironmentSpec.swift
@@ -0,0 +1,13 @@
+
+import Quick
+import Nimble
+@testable import NayutayaToolbox
+
+class DeviceEnvironmentSpec : QuickSpec {
+    override func spec() {
+        describe("") {
+            it("") {
+            }
+        }
+    }
+}

f17ef47e6e798b6578a0b423c2bbc0e3ef375df0
Yuya Kato <yuyakato@gmail.com>
2015-10-23 17:58:45 +0900

    3種類の時刻源クラスを追加

--- NayutayaToolbox/TimeSource.swift
+++ NayutayaToolbox/TimeSource.swift
@@ -1,2 +1,31 @@
 
 import Foundation
+
+// 時刻源プロトコル
+protocol TimeSource {
+    var time: Int64 { get }
+}
+
+// UNIX時間（単位：ミリ秒）を用いる時刻源クラス
+public class UnixTimeSource : NSObject, TimeSource {
+    public var time: Int64 {
+        return Int64(NSDate().timeIntervalSince1970 * 1000)
+    }
+}
+
+// システム起動からの時間（単位：ミリ秒）を用いる時刻源クラス
+public class ElapsedTimeSource : NSObject, TimeSource {
+    public var time: Int64 {
+        let processInfo = NSProcessInfo.processInfo()
+        return Int64(processInfo.systemUptime * 1000)
+    }
+}
+
+// テスト用の模擬時刻源クラス
+public class MockTimeSource : NSObject, TimeSource {
+    public var time: Int64
+
+    init(_ time: Int64) {
+        self.time = time
+    }
+}

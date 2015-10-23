40b72a9327b4e09d4a197e6d54ca4b24f9c80d5b
Yuya Kato <yuyakato@gmail.com>
2015-10-23 16:59:39 +0900

    ひな形を追加

---
+++ NayutayaToolboxTests/TimeSourceSpec.swift
@@ -0,0 +1,13 @@
+
+import Quick
+import Nimble
+@testable import NayutayaToolbox
+
+class TimeSourceSpec : QuickSpec {
+    override func spec() {
+        describe("") {
+            it("") {
+            }
+        }
+    }
+}
